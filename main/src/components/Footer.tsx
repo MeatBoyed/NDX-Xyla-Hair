886156fcb5a4386076eacc4dba8525c16f64bd91
MeatBoyed <charlierossouw@outlook.com>
2024-11-06 18:49:17 +0200

    feat: Branding added to footer

--- main/src/components/Footer.tsx
+++ main/src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link"
 
+import BrandingFooter from "./ndx/branding-footer"
+
 export default function FooterSection() {
   return (
-    <footer className="bg-primary py-8 text-white">
+    <footer className="space-y-16 bg-primary py-8 text-white">
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-between px-6 md:flex-row">
-        <p>&copy; 2024 Rivonia Hair & Nail Care. All rights reserved.</p>
+        <p>Rivonia Hair & Nail Care</p>
         <div className="mt-4 flex space-x-4 md:mt-0">
           <Link href="#" className="hover:text-accent">
             Privacy Policy
@@ -14,6 +16,7 @@ export default function FooterSection() {
           </Link>
         </div>
       </div>
+      <BrandingFooter />
     </footer>
   )
 }
--- main/src/components/ndx/branding-footer.tsx
+++ main/src/components/ndx/branding-footer.tsx
@@ -1,17 +1,20 @@
 export default function BrandingFooter() {
   return (
-    <div className="w-full  min-h-fit flex justify-center items-center flex-col gap-5">
-      <div className="flex justify-center items-center flex-col">
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Powered by</h3>
+    <div className="flex  min-h-fit w-full flex-col items-center justify-center gap-5">
+      <div className="flex flex-col items-center justify-center">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Powered by
+        </h3>
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight text-[#00AAFF] ">
           Nerf Designs
         </h2>
         <div className="leading-7">Nerf your competition</div>
       </div>
 
-      <p className="leading-7 text-center">
-        Copyright © {new Date().getFullYear()} Nerf Designs. All Rights Reserved
+      <p className="text-center leading-7">
+        Copyright © {new Date().getFullYear()} Nerf Designs. All Rights
+        Reserved
       </p>
     </div>
-  );
+  )
 }
