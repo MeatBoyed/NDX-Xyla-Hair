f35306eb50a6eba62bc4a7675163f7699920921b
MeatBoyed <charlierossouw@outlook.com>
2024-11-28 10:41:51 +0200

    fix: utils file

--- main/src/lib/utils.ts
+++ main/src/lib/utils.ts
@@ -1,12 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-import { env } from "@/env.mjs"
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function getImageUrl(objectKey: string) {
-  return `${env.NEXT_PUBLIC_CLOUDFRONT_BASEURL}/${FOLDER}/${objectKey}`
 }
